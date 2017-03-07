802ffd67e7e1c1991afc90a66d06531e5fb800d3
Radostan Riedel <raybuntu@googlemail.com>
2017-03-07 14:44:23 +0100

    inputstream.adaptive: bump version

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -17,7 +17,8 @@
 ################################################################################
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="cf75999"
+#PKG_VERSION="cf75999"
+PKG_VERSION="67a5e92"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/liberty-developer/inputstream.adaptive/archive/$PKG_VERSION.tar.gz"
