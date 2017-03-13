aabc3e2161c8a83130fb8a6782a2112283685f81
Radostan Riedel <raybuntu@googlemail.com>
2017-03-13 08:44:22 +0100

    kodi-agile: update to 040db82

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="6ff5744"
+PKG_VERSION="040db82"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
