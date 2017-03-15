5718441b9224b062a48e522fad7fe8b11e9f7a44
Radostan Riedel <raybuntu@googlemail.com>
2017-03-15 10:35:21 +0100

    kodi-agile: update to 1b15bf4

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="040db82"
+PKG_VERSION="1b15bf4"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
