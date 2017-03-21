41b1efed0725fab451edc9a91eee8e0c188c42da
Radostan Riedel <raybuntu@googlemail.com>
2017-03-21 11:02:31 +0100

    kodi-agile: update to 907eff3

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="a7477a5"
+PKG_VERSION="907eff3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
