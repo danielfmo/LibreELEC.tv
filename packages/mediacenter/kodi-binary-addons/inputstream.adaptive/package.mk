66fd815e407b4f7c65591060289955ea067af950
Radostan Riedel <raybuntu@googlemail.com>
2017-04-04 09:14:19 +0200

    inputstream.adaptive: update to 18e2f8d

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="3afbfba"
-PKG_REV="7"
+PKG_VERSION="18e2f8d"
+PKG_REV="8"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/liberty-developer/inputstream.adaptive/archive/$PKG_VERSION.tar.gz"
