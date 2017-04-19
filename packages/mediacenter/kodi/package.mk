0911853c4efdfdeea9cad4718e44c0c57aae98a2
Radostan Riedel <raybuntu@googlemail.com>
2017-04-19 08:55:46 +0200

    kodi-agile: update to 2f67109

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="856c99b"
+PKG_VERSION="2f67109"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
