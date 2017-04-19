ae7e9075aa6d07a541f82da4e3af984d721e31dc
Radostan Riedel <raybuntu@googlemail.com>
2017-04-19 14:36:27 +0200

    kodi: update to  5066f8f

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="fc82741"
+PKG_VERSION="5066f8f"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
