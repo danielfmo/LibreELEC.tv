60e371c515e4a058df893bcb62862ff6a58be70f
Radostan Riedel <raybuntu@googlemail.com>
2017-04-19 14:21:02 +0200

    inputstream.adaptive: update to a7e84de

--- packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
+++ packages/mediacenter/kodi-binary-addons/inputstream.adaptive/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="inputstream.adaptive"
-PKG_VERSION="734f684"
-PKG_REV="9"
+PKG_VERSION="a7e84de"
+PKG_REV="10"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/liberty-developer/inputstream.adaptive/archive/$PKG_VERSION.tar.gz"
