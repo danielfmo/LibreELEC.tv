c78a61d6e5d4723ea84d36c676108ef2a80e94e2
Radostan Riedel <raybuntu@googlemail.com>
2017-04-24 08:29:48 +0200

    kodi: update to 661dd08

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="5066f8f"
+PKG_VERSION="661dd08"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
