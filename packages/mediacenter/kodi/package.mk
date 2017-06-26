7411dad428176aab6baefc8900688155703dc723
Radostan Riedel <raybuntu@googlemail.com>
2017-06-26 10:53:00 +0200

    kodi: update to  fea89e7

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="423072a"
+PKG_VERSION="fea89e7"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
