1b9a79de757759599751fe0fe25c82414d2e09c9
Radostan Riedel <raybuntu@googlemail.com>
2017-07-16 11:13:28 +0200

    kodi: update to 1571c7f

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="ee0d877"
+PKG_VERSION="1571c7f"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
