02a5b6f0f5148a0445ea9e104f3932fda87a8f57
Radostan Riedel <raybuntu@googlemail.com>
2017-09-20 10:56:15 +0200

    Add theupdateframework (tuf) package
    Add new package pynacl
    Add new package python-cryptography
    Add new package python-securesystemslib
    Add new package pycparser
    Add new package python-asn1crypto
    Add new package python-enum34
    Add new package python-idna
    Add new package python-iso8601
    Add new package python-six
    Add new package libsodium
    Add new package python-natsort
    LibreELEC-settings: depend on tuf and python-natsort
    python-cryptography: link against pthreads
    pycryptodome: Fix cdef

---
+++ packages/python/security/tuf/package.mk
@@ -0,0 +1,52 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tuf"
+PKG_VERSION="0.10.0"
+PKG_SHA256="101b93ce59a5eb9c1f24a404625091b2530713170c80b0c857efd442fd0a7986"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://theupdateframework.github.io/"
+PKG_URL="https://github.com/theupdateframework/tuf/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host python-securesystemslib python-six python-iso8601"
+PKG_SECTION="python/security"
+PKG_SHORTDESC="The Update Framework"
+PKG_LONGDESC="TUF: Like the S in HTTPS, a plug-and-play library for securing a software updater"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_configure_target() {
+  cd $PKG_BUILD
+  rm -rf .$TARGET_NAME
+
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/bin
+}
---
+++ packages/security/libsodium/package.mk
@@ -0,0 +1,32 @@
+###############################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="libsodium"
+PKG_VERSION="1.0.13"
+PKG_SHA256="83c3017600bf18b349bfcf97cb8bf07e97ee2793743e860a83a0f08a2c251593"
+PKG_ARCH="any"
+PKG_LICENSE="ISC"
+PKG_SITE="https://libsodium.org/"
+PKG_URL="https://github.com/jedisct1/libsodium/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="security"
+PKG_SHORTDESC="Sodium is a modern, easy-to-use software library for encryption, decryption, signatures, password hashing and more."
+PKG_LONGDESC="Sodium is a modern, easy-to-use software library for encryption, decryption, signatures, password hashing and more."
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="yes"
